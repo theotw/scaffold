03359c309f428e6a0264e76f0744ae8ae03ed024
Brian Mason <brian.mason@slim.ai>
2024-02-28 10:51:20 -0500

    not helper function for errors

---
+++ error/wrapper.go
@@ -0,0 +1,14 @@
+package error
+
+import (
+	"fmt"
+)
+
+// WrapIfNotNil the lazy persons wrapper, if there is a nil for err, it returns nil,
+// if error has a value, then the error is wrapped with the message and returned
+func WrapIfNotNil(message string, err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("%s - %w", message, err)
+}
